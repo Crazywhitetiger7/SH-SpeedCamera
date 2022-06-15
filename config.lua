9660f9d8ec621d81823b5daf9c4723f0ac446866
Crazywhitetiger7 <70085037+Crazywhitetiger7@users.noreply.github.com>
2022-06-15 14:38:20 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,22 +1,29 @@
-Config = {} 
+Config = {}
 
-Config.useCameraSound = true
-Config.SpeedTypeMPH = true
-Config.PoliceNotfiy = true	
+Config.USE_CAMERA_SOUND = true
+Config.SPEED_TYPE_MPH = true
+Config.POLICE_NOTIFY = true
+
+Config.BLACK_LISTED_VEHICLES = {
+    ['POLICE'] = true,
+    ['POLICE2'] = true,
+}
 
 Config.SPEED_CAMERA = {
     {
         Coords = vector3(-474.62, -1780.43, 20.5),
-        MaxSpeed = Config.SpeedTypeMPH and 40 or 60,
-        SpeedLimit = Config.SpeedTypeMPH and '40MPH' or '60KM', 
-        AlertSpeed = Config.SpeedTypeMPH and 50 or 70,
+        MaxSpeed = Config.SPEED_TYPE_MPH and 40 or 60,
+        SpeedLimit = Config.SPEED_TYPE_MPH and '40MPH' or '60KM/H', 
+        AlertSpeed = Config.SPEED_TYPE_MPH and 50 or 70,
+        UseBlip = true,
         BillAmount = 200
     },
     {
         Coords = vector3(28.19, -1545.77, 28.91),
-        MaxSpeed = Config.SpeedTypeMPH and 40 or 60,
-        SpeedLimit = Config.SpeedTypeMPH and '40MPH' or '60KM', 
-        AlertSpeed = Config.SpeedTypeMPH and 50 or 70,
+        MaxSpeed = Config.SPEED_TYPE_MPH and 40 or 60,
+        SpeedLimit = Config.SPEED_TYPE_MPH and '40MPH' or '60KM/H', 
+        AlertSpeed = Config.SPEED_TYPE_MPH and 50 or 70,
+        UseBlip = true,
         BillAmount = 200
     }
 }
